7208faabd3a6834cbd11e54d7b25250721ff1e12
Caetano Santana <santanacaetano.m@gmail.com>
2023-07-31 12:16:26 -0400

    Alterações pagiona plantões

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "09bc3ff76e8f7c82"; }
+/******/ 		__webpack_require__.h = function() { return "2d2b88b2f034314e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- frontend/pages/plantoes/index.js
+++ frontend/pages/plantoes/index.js
@@ -16,48 +16,98 @@ const headers= {
     'Authorization': 'Bearer ceeb0dd52060307ab38137799d4f61d249602fb52e52b4c2f9343a743eaec40cffa447c0537093ff02c26a362bcfddf9cf196206f082ae2e7ceaaa2afea35c1c7c1b7ab527076ccc0b06f80428b5304723b6e77e0c460a24043e33d762585d75c0d1dcb7554598490b0edf6a1a41ce79381486a10281a42c245c80e4d1bfd54b'
 };
 
-
-    /*const data2 = await fetch('http://localhost:1337/api/juizs',{
-        method: 'GET',
-        headers
-    })
-    const juizes = await data2.json()
-
-    const data3 = await fetch('http://localhost:3000/api/escala')
-    const escalas = await data3.json()*/
-
 function Plantoes() {
 
     //------- CONSTANTES PARA O DATAGRID----------------------------------------
-    /*const [opcaoSelecionada, setOpcaoSelecionada] = useState(null);
-    const [select, setSelection] = React.useState([]);*/
+    const [opcaoSelecionada, setOpcaoSelecionada] = useState(null);
+    const [select, setSelection] = React.useState([]);
     //--------------------------------------------------------------------------
 
     //const [modifiedData, setModifiedData] = useState(valorInicial);//-------do it
     const [juizes, setJuizes] = useState([]);
+    const [escalas, setEscalas] = useState([]);
+    const [plantoes, setPlantoes] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchJuizes = async () => {
             try {
 
-                const response = await fetch('http://localhost:1337/api/juizs', {
+                const response1 = await fetch('http://localhost:1337/api/juizs', {
                     method: 'GET',
                     headers,
                 });
 
-                if (!response.ok) {
+                if (!response1.ok) {
                     throw new Error('Falha ao obter os dados dos juizes.');
                 }
 
-                const responseData = await response.json();
+                const responseJuiz = await response1.json();
                 console.log('')
                 console.log('------------------------------------------------')
-                console.log('-------| Constante DATA:', responseData);
+                console.log('-------| Constante responseJuiz:', responseJuiz);
 
-                if (Array.isArray(responseData.data)) {
-                    const juizesData = responseData.data.map((item) => ({id: item.id, ...item.attributes,}));
+                if (Array.isArray(responseJuiz.data)) {
+                    const juizesData = responseJuiz.data.map((item) => ({id: item.id, ...item.attributes,}));
                     setJuizes(juizesData);
+
+                } else {
+                    setError('Formato de dados inválido.');
+                }
+
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+        const fetchEscalas = async () => {
+            try {
+
+                const response2 = await fetch('http://localhost:1337/api/escalas', {
+                    method: 'GET',
+                    headers,
+                });
+
+                if (!response2.ok) {
+                    throw new Error('Falha ao obter os dados dos juizes.');
+                }
+
+                const responseEscala = await response2.json();
+                console.log('')
+                console.log('------------------------------------------------')
+                console.log('-------| Constante responseEscala:', responseEscala);
+
+                if (Array.isArray(responseEscala.data)) {
+                    const escalasData = responseEscala.data.map((item) => ({id: item.id, ...item.attributes,}));
+                    setEscalas(escalasData);
+
+                } else {
+                    setError('Formato de dados inválido.');
+                }
+
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+        const fetchPlantoes = async () => {
+            try {
+
+                const response3 = await fetch('http://localhost:1337/api/plantoes', {
+                    method: 'GET',
+                    headers,
+                });
+
+                if (!response3.ok) {
+                    throw new Error('Falha ao obter os dados dos juizes.');
+                }
+
+                const responsePlantoes = await response3.json();
+                console.log('')
+                console.log('------------------------------------------------')
+                console.log('-------| Constante responsePlantoes:', responsePlantoes);
+
+                if (Array.isArray(responsePlantoes.data)) {
+                    const plantoesData = responsePlantoes.data.map((item) => ({id: item.id, ...item.attributes,}));
+                    setPlantoes(plantoesData);
 
                 } else {
                     setError('Formato de dados inválido.');
@@ -69,12 +119,14 @@ function Plantoes() {
         };
 
         fetchJuizes();
+        fetchEscalas();
+        fetchPlantoes();
     }, []);
 
     /*const filterEscalas = (escalas, { inputValue }) => {
         const inputValueLowerCase = inputValue.toLowerCase();
         return escalas.filter(escala =>
-            escala.status.toLowerCase() === '0'
+            escala.fechada.toLowerCase() === false
         );
     };*///Constante para filtrar as escalas
 
@@ -103,7 +155,9 @@ function Plantoes() {
                     <h5>Selecione o nome do juiz e a escala:</h5>
                     <h2>
                         {console.log('-------| PÓS MAP |--------')}
-                        {console.log('-------| Constante Escalas:', juizes)}
+                        {console.log('-------| Constante Juizes:', juizes)}
+                        {console.log('-------| Constante Escalas:', escalas)}
+                        {console.log('-------| Constante Plantoes:', plantoes)}
                         {console.log('')}
                     </h2>
                 </MDBox>
@@ -122,7 +176,7 @@ function Plantoes() {
                                 <Autocomplete
                                     options={escalas}
                                     getOptionLabel={escala => escala.descricao}
-                                    filterOptions={filterEscalas}
+
                                     value={opcaoSelecionada}
                                     onChange={(event, newValue) => setOpcaoSelecionada(newValue)}
                                     renderInput={(params) => <TextField {...params} label="Escala" />}
@@ -136,7 +190,7 @@ function Plantoes() {
                             <Grid item xs={12} sm={5}>
                                 <Grid style={{ flex: '1' }}>
 
-                                    {opcaoSelecionada && opcaoSelecionada.datasplantao && (
+                                    {opcaoSelecionada && (
                                         <DataGrid
                                             checkboxSelection
                                             disableColumnMenu
@@ -145,9 +199,8 @@ function Plantoes() {
                                             initialState={{pagination: { paginationModel: { pageSize: 5 } },}}
                                             isRowSelectable={(params) => params.row.status === true}
                                             onSelectionChange={(newSelection) => {setSelection(newSelection.rows);}}
-                                            rows={opcaoSelecionada.datasplantao}
-                                            columns={[{field:'data', headerName:'Datas',width: 120, sortable:false},{
-                                                field: 'status', headerName: 'Status', width: 120,
+                                            rows={plantoes}
+                                            columns={[{field:'data', headerName:'Datas',width: 120, sortable:false},{field:'escala', headerName:'Escala',width: 120, sortable:false},{field:'juiz', headerName:'Juiz',width: 120, sortable:false},{field: 'status', headerName: 'Status', width: 120,
                                                 renderCell: (params) => (
                                                     <span style={{ color: params.value ? 'green' : 'red' }}>
                                                         {params.value ? 'Disponível' : 'Ocupado'}
