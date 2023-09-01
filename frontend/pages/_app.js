2be42f2453994cde7ebd19d7f8089a3d646c9e38
Magson Magalhaes <magson@gmail.com>
2023-09-01 16:39:44 -0400

    Corrige TITLE em _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -187,7 +187,7 @@ function MyApp({
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="shortcut icon" href={favicon.src} />
           <link rel="apple-touch-icon" sizes="76x76" href={appleIcon.src} />
-          <title>Next Material Dashboard 2 PRO</title>
+          <title>Plantão App</title>
         </Head>
         <Main Component={Component} pageProps={pageProps} />
       </CacheProvider>
