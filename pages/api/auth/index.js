08617c6b1b47f641fc427da45d941468c739cc5a
Caetano Santana <santanacaetano.m@gmail.com>
2024-03-21 11:11:36 -0400

    feat: configura autenticação na página de login

--- pages/api/auth/index.js
+++ pages/api/auth/index.js
@@ -1,17 +1,20 @@
 import jwt from 'jsonwebtoken';
+import { setCookie } from 'nookies';
+
 
 export default async function action(request, response) {
 
-    const {username, password} = request.body;
+    const {email, password} = request.body;
+    console.log("request: ",request,"response: ",response)
+
+    const username = email;
 
     let config = {
         url: process.env.LDAP_SERVER, // Endereço do Servidor LDAP / AD
         baseDN: process.env.LDAP_BASEDN,
         attributes: {
             user: ['cn','sAMAccountName','sn', 'givenName', 'mail','trfCPF','displayName'],
-            group: [
-                'dn', 'cn', 'description', 'distinguishedName', 'objectCategory'
-            ]
+            group: ['dn', 'cn', 'description', 'distinguishedName', 'objectCategory']
         },
         username,
         password
@@ -26,6 +29,12 @@ export default async function action(request, response) {
             }
             if (auth){
                 const token = generateAccessToken({username})
+
+                // Defina o cookie com o token de acesso
+                setCookie(undefined, 'access_token', token, {
+                    maxAge: 1800, // Tempo de vida do cookie em segundos (1800 segundos = 30 minutos)
+                });
+
                 response.status(200).json({ auth, token})
             } else {
                 response.status(400).json({ message: 'crendenciais invalidas', auth: false });
--- pages/authentication/login/index.js
+++ pages/authentication/login/index.js
@@ -1,28 +1,12 @@
 import { useState } from "react";
-
 import Link from "next/link";
-
-// @mui material components
 import Card from "@mui/material/Card";
-import Switch from "@mui/material/Switch";
 import Grid from "@mui/material/Grid";
-import MuiLink from "@mui/material/Link";
-
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import GoogleIcon from "@mui/icons-material/Google";
-
-// NextJS Material Dashboard 2 PRO components
 import MDBox from "/components/MDBox";
 import MDTypography from "/components/MDTypography";
 import MDInput from "/components/MDInput";
 import MDButton from "/components/MDButton";
-
-// Authentication layout components
 import BasicLayout from "/pagesComponents/authentication/components/BasicLayout";
-
-// Images
 import bgImage from "/assets/images/bg-sign-in-basic.jpeg";
 
 
@@ -30,20 +14,37 @@ function LoginForm() {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
-    })
+    });
 
-    const handleChange = e =>{
+    const handleChange = e => {
         const { name, value } = e.target;
         setFormData({
             ...formData,
-            [name]:value
-        })
-    }
+            [name]: value
+        });
+    };
 
-    const handleSubmit = e => {
+    const handleSubmit = async e => {
         e.preventDefault();
-        console.log(formData)
-    }
+        try {
+            const res = await fetch("/api/auth", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email: formData.email, password: formData.password })
+            });
+            if (res.ok) {
+                const data = await res.json();
+                window.location.href = '/plantoes';
+                console.log(data);
+            } else {
+                console.error("Erro ao autenticar:", res.statusText);
+            }
+        } catch (error) {
+            console.error("Erro ao enviar solicitação:", error);
+        }
+    };
 
     return (
         <BasicLayout image={bgImage}>
@@ -62,35 +63,19 @@ function LoginForm() {
                     <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
                         Autenticar
                     </MDTypography>
-                    <Grid
-                        container
-                        spacing={3}
-                        justifyContent="center"
-                        sx={{ mt: 1, mb: 2 }}
-                    >
-
-
-
-                    </Grid>
                 </MDBox>
                 <MDBox pt={4} pb={3} px={3}>
                     <MDBox component="form" role="form">
                         <MDBox mb={2}>
-                            <MDInput type="email" value={formData.email} id="email" name="email" label="Email" fullWidth onChange={handleChange}/>
+                            <MDInput type="email" value={formData.email} id="email" name="email" label="Email" fullWidth onChange={handleChange} />
                         </MDBox>
                         <MDBox mb={2}>
-                            <MDInput type="password" value={formData.password} id="password" name="password" label="Password" fullWidth onChange={handleChange}/>
-                        </MDBox>
-                        <MDBox display="flex" alignItems="center" ml={-1}>
-
+                            <MDInput type="password" value={formData.password} id="password" name="password" label="Password" fullWidth onChange={handleChange} />
                         </MDBox>
                         <MDBox mt={4} mb={1}>
                             <MDButton variant="gradient" color="dark" fullWidth onClick={handleSubmit}>
                                 LOGIN
                             </MDButton>
-                        </MDBox>
-                        <MDBox mt={3} mb={1} textAlign="center">
-
                         </MDBox>
                     </MDBox>
                 </MDBox>
