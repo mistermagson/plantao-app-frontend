9244ccc59a650d4793cf233a375c33f0f43cb8ad
Caetano Santana <santanacaetano.m@gmail.com>
2024-05-17 12:23:10 -0400

    fix: corrige erro de image em login page

--- pagesComponents/authentication/components/BasicLayout/index.js
+++ pagesComponents/authentication/components/BasicLayout/index.js
@@ -12,7 +12,7 @@ import MDBox from "/components/MDBox";
 
 import PageLayout from "/examples/LayoutContainers/PageLayout";
 
-function BasicLayout({ image, children }) {
+function BasicLayout({  children }) {
 
 
   return (
@@ -22,20 +22,7 @@ function BasicLayout({ image, children }) {
         position="absolute"
         width="100%"
         minHeight="100vh"
-        sx={{
-          backgroundImage: ({
-            functions: { linearGradient, rgba },
-            palette: { gradients },
-          }) =>
-            image &&
-            `${linearGradient(
-              rgba(gradients.dark.main, 0.6),
-              rgba(gradients.dark.state, 0.6),
-            )}, url(${image.src || image})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
+
       />
       <MDBox px={1} width="100%" height="100vh" mx="auto">
         <Grid
@@ -57,7 +44,7 @@ function BasicLayout({ image, children }) {
 
 // Typechecking props for the BasicLayout
 BasicLayout.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+
   children: PropTypes.node.isRequired,
 };
 
