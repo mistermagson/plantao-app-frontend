1194675b335f8d983bc1c75eceb7dcec549a8148
Caetano Santana <santanacaetano.m@gmail.com>
2024-09-26 13:28:14 -0400

    feat: adiciona fetch de varas e implementa componente de adição de juizes

--- pages/escalas/participantes/index.js
+++ pages/escalas/participantes/index.js
@@ -21,6 +21,7 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import {validateAdmin, validateAuthToken} from "../../../utils/sistemaUtils";
 import {parseCookies} from "nookies";
 import MDButton from "../../../components/MDButton";
+import NovoJuizForm from "../novoJuizForm";
 
 const headers = {
     'Content-Type': 'application/json',
@@ -33,6 +34,7 @@ function Participantes() {
     const [rowSelectionModel, setRowSelectionModel] = React.useState([]);
     //--------------------------------------------------------------------------
     const [escalas, setEscalas] = useState([]);
+    const [varas, setVaras] = useState([]);
     const [juizes, setJuizes] = useState([]);
     const [juizesFiltro, setJuizesFiltro] = useState([]);
     const [adicionados, setAdicionados] = useState([]);
@@ -72,6 +74,7 @@ function Participantes() {
             setError(error.message);
         }
     };
+
     const fetchEscalas = async () => {
         try {
             const response2 = await fetch(`http://${process.env.NEXT_PUBLIC_STRAPI_HOST}:1337/api/escalas?populate[participantes][populate][]=plantoes&populate[preferencia][populate][]=juizs&populate[participantes][populate][]=lotacao`, {
@@ -100,9 +103,28 @@ function Participantes() {
         }
     };
 
+    const fetchVaras = async () => {
+        try {
+            const response = await fetch(`http://${process.env.NEXT_PUBLIC_STRAPI_HOST}:1337/api/varas\\`);
+            const data = await response.json();
+
+            // Mapeia os dados recebidos e extrai apenas o id e descrição
+            const varasExtraidas = data.data.map((vara) => ({
+                id: vara.id,
+                descricao: vara.attributes.descricao,
+            }));
+
+            // Atualiza o estado com o array contendo id e descrição
+            setVaras(varasExtraidas);
+        } catch (error) {
+            console.error('Erro ao buscar as varas:', error);
+        }
+    };
+
     useEffect( () => {
         fetchEscalas()
         fetchJuizes()
+        fetchVaras()
     }, []);
 
 
@@ -261,8 +283,9 @@ function Participantes() {
                 <h1>Lista de Participantes</h1>
                 {/*<MDButton size="small" onClick={()=>showJSON()} lcolor="info">Exibir</MDButton>*/}
             </MDBox>
-
-            <Card sx={{height: "100%"}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={8} xl={8}>
+                    <Card>
                 <Grid container spacing={2} p={4}>
                     <Grid item xs={12} md={5} xl={5}>
                         <MDBox py={2}>
@@ -281,8 +304,6 @@ function Participantes() {
                             renderInput={(params) => <TextField {...params} label="Escala"/>}
                         />
                     </Grid>
-
-
                 </Grid>
                 <Grid container spacing={6} p={4}>
                     <Grid>
@@ -311,114 +332,120 @@ function Participantes() {
                                     <MDTypography variant="h6" sx={{ fontWeight: 'regular'}}>Apenas adicionados</MDTypography >
                                 </label>
                                 </Grid>
-                            <DataGrid
-                                density="compact"
-                                editMode="row"
-                                disableColumnMenu
-                                sx={{fontSize: '16px', fontWeight: 'regular', padding: '10px', }}
-                                style={{height: '500px'}}
-                                pageSizeOptions={[5, 10, 20,50,100]}
-                                initialState={{
-                                    pagination: {paginationModel: {pageSize: 100}},
-                                    sorting: {sortModel: [{field: 'antiguidade', sort: 'asc'}],},
-                                }}
-                                rows={juizesFiltrados}
-                                columns={[
-                                    {
-                                        field: 'opcoes',
-                                        headerName: 'Opções',
-                                        minWidth: 80,
-                                        renderCell: (params) => (
-                                            <div>
-                                                <Tooltip title={isJuizPreferencial(params.row.id) ? 'Escolhendo...' : 'Definir como Preferencial'}>
-                                                    <GridActionsCellItem
-                                                        icon={<span className="material-icons-outlined">ads_click</span>}
-                                                        label={isJuizPreferencial(params.row.id) ? 'Escolhendo...' : 'Definir como Preferencial'}
-                                                        onClick={() => {
-                                                            if (!isJuizPreferencial(params.row.id)) {
-                                                                handleAlterarPreferencia(params.row)
-                                                            }
-                                                        }}
-                                                        color={isJuizPreferencial(params.row.id) ? 'error' : 'default'}
-                                                    />
-                                                </Tooltip>
-                                                {isJuizAdicionado(params.row.id) ? (
-                                                    <Tooltip title="">
+                                <DataGrid
+                                    density="compact"
+                                    editMode="row"
+                                    disableColumnMenu
+                                    sx={{fontSize: '16px', fontWeight: 'regular', padding: '10px', }}
+                                    style={{height: '500px'}}
+                                    pageSizeOptions={[5, 10, 20,50,100]}
+                                    initialState={{
+                                        pagination: {paginationModel: {pageSize: 100}},
+                                        sorting: {sortModel: [{field: 'antiguidade', sort: 'asc'}],},
+                                    }}
+                                    rows={juizesFiltrados}
+                                    columns={[
+                                        {
+                                            field: 'opcoes',
+                                            headerName: 'Opções',
+                                            minWidth: 80,
+                                            renderCell: (params) => (
+                                                <div>
+                                                    <Tooltip title={isJuizPreferencial(params.row.id) ? 'Escolhendo...' : 'Definir como Preferencial'}>
                                                         <GridActionsCellItem
-                                                            icon={<RemoveCircleOutlineIcon />}
-                                                            label="Remover Juiz"
-                                                            onClick={() => handleLimparParticipante(params.row.id)}
-                                                            color="inherit"
-                                                        />
-                                                    </Tooltip>
-                                                ) : (
-                                                    <Tooltip title="Adicionar Juiz">
-                                                        <GridActionsCellItem
-                                                            icon={<AddCircleOutlineIcon />}
-                                                            label="Adicionar Juiz"
+                                                            icon={<span className="material-icons-outlined">ads_click</span>}
+                                                            label={isJuizPreferencial(params.row.id) ? 'Escolhendo...' : 'Definir como Preferencial'}
                                                             onClick={() => {
-                                                                console.log('ADD',params.row)
-                                                                handleSubmit(params.row.id)
-                                                            }} // Chame a função para adicionar juiz aqui
-                                                            color="inherit" // Use 'primary' para destacar a opção de adicionar
+                                                                if (!isJuizPreferencial(params.row.id)) {
+                                                                    handleAlterarPreferencia(params.row)
+                                                                }
+                                                            }}
+                                                            color={isJuizPreferencial(params.row.id) ? 'error' : 'default'}
                                                         />
                                                     </Tooltip>
-                                                )}
-                                            </div>
-                                        ),
-                                    },
-                                    {
-                                        field: 'antiguidade',
-                                        headerName: 'RF',
-                                        flex: 0.05,
-                                        renderCell: (params) => (
-                                            <div style={{ color: isJuizAdicionado(params.row.id) ? 'green' : 'black' }}>
-                                                {params.row.antiguidade}
-                                            </div>
-                                        ),
-                                    },
-                                    {
-                                        field: 'nome',
-                                        headerName: 'Nome',
-                                        flex: 0.7,
-                                        minWidth: 150,
-                                        renderCell: (params) => (
-                                            <div style={{ color: isJuizAdicionado(params.row.id) ? 'green' : 'black' }}>
-                                                {params.row.nome}
-                                            </div>
-                                        ),
-                                    },
-                                    {
-                                        field: 'lotacao.data.attributes.descricao',
-                                        headerName: 'Vara',
-                                        flex: 1,
-                                        minWidth: 150,
-                                        valueGetter: (params) => {
-                                            return params.row.lotacao?.data?.attributes?.descricao;
+                                                    {isJuizAdicionado(params.row.id) ? (
+                                                        <Tooltip title="">
+                                                            <GridActionsCellItem
+                                                                icon={<RemoveCircleOutlineIcon />}
+                                                                label="Remover Juiz"
+                                                                onClick={() => handleLimparParticipante(params.row.id)}
+                                                                color="inherit"
+                                                            />
+                                                        </Tooltip>
+                                                    ) : (
+                                                        <Tooltip title="Adicionar Juiz">
+                                                            <GridActionsCellItem
+                                                                icon={<AddCircleOutlineIcon />}
+                                                                label="Adicionar Juiz"
+                                                                onClick={() => {
+                                                                    console.log('ADD',params.row)
+                                                                    handleSubmit(params.row.id)
+                                                                }} // Chame a função para adicionar juiz aqui
+                                                                color="inherit" // Use 'primary' para destacar a opção de adicionar
+                                                            />
+                                                        </Tooltip>
+                                                    )}
+                                                </div>
+                                            ),
                                         },
-                                        renderCell: (params) => (
-                                            <div style={{ color: isJuizAdicionado(params.row.id) ? 'green' : 'black' }}>
-                                                {params.row.lotacao?.data?.attributes?.descricao}
-                                            </div>
-                                        ),
-                                    },
+                                        {
+                                            field: 'antiguidade',
+                                            headerName: 'RF',
+                                            flex: 0.05,
+                                            renderCell: (params) => (
+                                                <div style={{ color: isJuizAdicionado(params.row.id) ? 'green' : 'black' }}>
+                                                    {params.row.antiguidade}
+                                                </div>
+                                            ),
+                                        },
+                                        {
+                                            field: 'nome',
+                                            headerName: 'Nome',
+                                            flex: 0.7,
+                                            minWidth: 150,
+                                            renderCell: (params) => (
+                                                <div style={{ color: isJuizAdicionado(params.row.id) ? 'green' : 'black' }}>
+                                                    {params.row.nome}
+                                                </div>
+                                            ),
+                                        },
+                                        {
+                                            field: 'lotacao.data.attributes.descricao',
+                                            headerName: 'Vara',
+                                            flex: 1,
+                                            minWidth: 150,
+                                            valueGetter: (params) => {
+                                                return params.row.lotacao?.data?.attributes?.descricao;
+                                            },
+                                            renderCell: (params) => (
+                                                <div style={{ color: isJuizAdicionado(params.row.id) ? 'green' : 'black' }}>
+                                                    {params.row.lotacao?.data?.attributes?.descricao}
+                                                </div>
+                                            ),
+                                        },
 
-                                ]}
-                                onRowSelectionModelChange={(newRowSelectionModel) => {setRowSelectionModel(newRowSelectionModel);}}
-                                rowSelectionModel={rowSelectionModel}
-                                disableColumnSelector
-                                disableDensitySelector
-                                slots={{toolbar: GridToolbar}}
-                                slotProps={{toolbar: {showQuickFilter: true,},}}
+                                    ]}
+                                    onRowSelectionModelChange={(newRowSelectionModel) => {setRowSelectionModel(newRowSelectionModel);}}
+                                    rowSelectionModel={rowSelectionModel}
+                                    disableColumnSelector
+                                    disableDensitySelector
+                                    slots={{toolbar: GridToolbar}}
+                                    slotProps={{toolbar: {showQuickFilter: true,},}}
 
-                            />
+                                />
                             </div>)}
 
 
                     </Grid>
                 </Grid>
             </Card>
-
+                </Grid>
+                <Grid item xs={12} md={4} xl={4} >
+                    <Card sx={{overflow: "visible"}}>
+                        <NovoJuizForm varas={varas}/>
+                    </Card>
+                </Grid>
+            </Grid>
         </DashboardLayout>
     );
 }
@@ -445,6 +472,6 @@ export async function getServerSideProps(ctx) {
 
     const escalas = "teste"
 
-    return { props: { data:escalas, validation: 'ok', tipo: cookies.user_tipo} }
+    return { props: { data:escalas, validation: 'ok', tipo: ''} }
 }
 export default Participantes;
